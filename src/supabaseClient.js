82b35c1e893194f7b2499b9198371c2b8fcf15c5
alainmtz <melvinalvin.bello@gmail.com>
2025-07-05 16:03:44 -0700

    Solving problems

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,4 +1,5 @@
 import { createClient } from '@supabase/supabase-js';
+import { Analytics } from "@vercel/analytics/next";
 
 const supabaseUrl = 'https://hukgwplgzeqjdljatxmy.supabase.co';
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh1a2d3cGxnemVxamRsamF0eG15Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA4NTkzMzIsImV4cCI6MjA2NjQzNTMzMn0.ydgVf36nFRd2EuRF2YdrVQgBxzyNlZfRpj2Z_zmYylA';
